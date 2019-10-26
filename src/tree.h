47708b9dde3f9c663afa550ecfaeb05849667a41
Phelps <phelpsmemo@gmail.com>
2019-10-25 21:26:28 -0300

    Add comments in code

--- src/tree.h
+++ src/tree.h
@@ -10,6 +10,15 @@ typedef struct ax {
     struct ax * left, * right;
 } Node;
 
+/**
+ * Function: tree
+ * -----------------
+ * Função para criar um nó raiz (Separação apenas semântica)
+ * 
+ * key: Valor inserido no nó criado
+ * 
+ * returns: Nó criado
+ */
 Node * tree(int key) {
     Node * tmp = (Node *) malloc(sizeof(Node));
     tmp->key = key;
@@ -17,6 +26,15 @@ Node * tree(int key) {
     return tmp;
 }
 
+/**
+ * Function: node
+ * -----------------
+ * Função para criar um nó na árvore
+ * 
+ * key: Valor inserido no nó criado
+ * 
+ * returns: Nó criado
+ */
 Node * node(int key) {
     Node * tmp = (Node *) malloc(sizeof(Node));
     tmp->key = key;
@@ -24,6 +42,16 @@ Node * node(int key) {
     return tmp;
 }
 
+/**
+ * Function: insert
+ * -----------------
+ * Função para inserir um nó na árvore binária
+ * 
+ * root: Ponteiro para o Node que representa a raiz da árvore
+ * newNode: Ponteiro para o novo nó que será inserido
+ * 
+ * returns: Devolve o nó inserido na árvore
+ */
 Node * insert(Node * root, Node * newNode) {
     if (root == NULL) return(newNode);
     
@@ -34,6 +62,16 @@ Node * insert(Node * root, Node * newNode) {
     }
 }
 
+/**
+ * Function: search
+ * -----------------
+ * Função para realizar pesquisa em uma árvore binária
+ * 
+ * element: Número inteiro pesquisado na árvore binária
+ * root: Ponteiro para o Node que representa a raiz da árvore
+ * 
+ * returns: Nó com o elemento encontrado, caso não encontre retorna NULL.
+ */
 Node * search(int element, Node * root) {
     if (root == NULL) return NULL;
     if (root->key == element) return root;
@@ -42,6 +80,16 @@ Node * search(int element, Node * root) {
     else return search(element, root->right);
 }
 
+/**
+ * Function: preorder
+ * -----------------
+ * Função para visualizar uma árvore binária, fazendo o percurso de exibição 
+ * em pré-ordem
+ * 
+ * root: Ponteiro para o Node que representa a raiz da árvore
+ * 
+ * returns: void
+ */
 void preorder(Node * root) {
     printf("%d\n", root->key);
     
@@ -54,6 +102,17 @@ void preorder(Node * root) {
     }
 }
 
+/**
+ * Function: inorder
+ * -----------------
+ * Função para visualizar uma árvore binária, fazendo o percurso de exibição 
+ * em ordem simétrica (in-ordem)
+ * 
+ * root: Ponteiro para o Node que representa a raiz da árvore
+ * 
+ * returns: void
+ * 
+ */
 void inorder(Node * root) {
     if (root->left != NULL) {
         inorder(root->left);
@@ -68,9 +127,11 @@ void inorder(Node * root) {
 /**
  * Function: height
  * -----------------
- * Função para calcular a altura de uma árvore binária. A altura 
+ * Função para calcular a altura de uma árvore binária. A altura da árvore
+ * é apresentada pelo nó de nível máximo, onde o nível representa a distância
+ * entre o nó e a raiz.
  * 
- * root: Ponteiro para o Node que representa a raiz do projeto
+ * root: Ponteiro para o Node que representa a raiz da árvore
  * 
  * returns: Inteiro representando a altura da árvore binária
  * 
@@ -91,10 +152,34 @@ int height(Node * root) {
     return hleft + 1;
 }
 
+/**
+ * Function: count
+ * -----------------
+ * Função para contar a quantidade de nós em uma árvore
+ * 
+ * root: Ponteiro para o Node que representa a raiz da árvore
+ * fncp: Ponteiro para uma função que recebe um nó e devolve um valor inteiro.
+ * Esta função será utilizada para realizar o cálculo. Funções como by_preorder, 
+ * by_inorder ou by_posorder podem ser aplicadas neste contexto.
+ * 
+ * returns: Inteiro representando a quantidade de nós na árvore
+ * 
+ */
 int count(Node * root, int (*fncp)(Node *)) {
     return fncp(root);
 }
 
+/**
+ * Function: by_preorder
+ * -----------------------
+ * Função auxiliar para a contagem de elementos em uma árvore binária qualquer.
+ * Utiliza o método pré-ordem para percorrer a árvore
+ * 
+ * root: Ponteiro para o Node que representa a raiz da árvore
+ * 
+ * returns: Inteiro representando a quantidade de nós na árvore
+ * 
+ */
 int by_preorder(Node * root) {
     if (!root) return 0;
 
@@ -103,6 +188,16 @@ int by_preorder(Node * root) {
     );
 }
 
+/**
+ * Function: by_inorder
+ * ----------------------
+ * Função auxiliar para a contagem de elementos em uma árvore binária qualquer.
+ * Utiliza o método in-ordem (Simétrica) para percorrer a árvore
+ * 
+ * root: Ponteiro para o Node que representa a raiz da árvore
+ * 
+ * returns: Inteiro representando a quantidade de nós na árvore
+ */
 int by_inorder(Node * root) {
     if (!root) return 0;
 
@@ -111,6 +206,16 @@ int by_inorder(Node * root) {
     );
 }
 
+/**
+ * Function: by_posorder
+ * ----------------------
+ * Função auxiliar para a contagem de elementos em uma árvore binária qualquer.
+ * Utiliza o método pós-ordem (depth-first) para percorrer a árvore
+ * 
+ * root: Ponteiro para o Node que representa a raiz da árvore
+ * 
+ * returns: Inteiro representando a quantidade de nós na árvore
+ */
 int by_posorder(Node * root) {
     if (!root) return 0;
 
